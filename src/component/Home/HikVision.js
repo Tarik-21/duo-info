19c41c441d94070b77c06d543c756ab5c1c21729
Tarik Mouhouch <tarikmouhouch9@gmail.com>
2022-10-03 10:35:13 +0100

    Save

--- src/component/Home/HikVision.js
+++ src/component/Home/HikVision.js
@@ -26,7 +26,7 @@ const HikVision = () => {
 
       <Slider {...settings}>
         <div className="w-full">
-            <img src='./images/hikvision/1.jpeg' alt="img" className="w-full lg:h-96 md:h-64"/>
+            <img src='./images/hikvision/1.png' alt="img" className="w-full lg:h-96 md:h-64"/>
         </div>
         <div className="w-full">
             <img src='./images/hikvision/2.png' alt="img" className="w-full lg:h-96 md:h-64"  />
